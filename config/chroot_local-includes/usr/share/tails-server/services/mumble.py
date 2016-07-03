60963ba9a58263d8f5d615eee849dc0e72d65a1d
segfault <segfault@riseup.net>
2016-07-03 12:50:16 +0000

    Rework mumble connection info

--- config/chroot_local-includes/usr/share/tails-server/services/mumble.py
+++ config/chroot_local-includes/usr/share/tails-server/services/mumble.py
@@ -80,10 +80,12 @@ class MumbleServer(service_template.TailsService):
         if not self.address:
             return None
 
-        # return "mumble://nickname:%s@%s" % (self.options_dict["server-password"].value,
-        #                                     self.address)
+        mime_string = "mumble://username:%s@%s" % (self.options_dict["server-password"].value,
+                                                   self.address)
+        if self.virtual_port != self.default_target_port:
+            mime_string += ":%s" % self.virtual_port
 
-        s = str()
+        s = "%s\n" % mime_string
         s += "Address: %s\n" % self.address
         s += "Port: %s\n" % self.virtual_port
         s += "Password: %s\n" % self.options_dict["server-password"].value
