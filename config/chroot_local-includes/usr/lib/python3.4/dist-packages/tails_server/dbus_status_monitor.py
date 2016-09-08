6eb29db3686c073d2b8435db1406dbd93629323c
segfault <segfault@riseup.net>
2016-09-08 12:43:19 +0000

    Listen to PropertiesChanged in addition to UnitNew in dbus_status_monitor.py

--- config/chroot_local-includes/usr/lib/python3.4/dist-packages/tails_server/dbus_status_monitor.py
+++ config/chroot_local-includes/usr/lib/python3.4/dist-packages/tails_server/dbus_status_monitor.py
@@ -32,11 +32,13 @@ listening = False
 
 
 def add_unit(unit_name, receiver_function):
+    logging.debug("Adding %r to D-Bus monitored units", unit_name)
     unit = SystemdUnit(unit_name, receiver_function)
     units[unit.path] = unit
 
 
 def remove_unit(unit_name):
+    logging.debug("Removing %r from D-Bus monitored units", unit_name)
     unit_path = manager.LoadUnit(unit_name)
     del units[unit_path]
 
@@ -45,6 +47,29 @@ def start_listening():
     global listening
     if listening:
         raise ReceiverAlreadyAddedError()
+    add_unit_loaded_receiver()
+
+    for unit_path in units:
+        add_properties_changed_receiver(unit_path)
+
+    listening = True
+    # This is only needed for debugging, usually the GLib.MainLoop will be started by the GUI
+    # loop.run()
+
+
+def stop_listening():
+    global listening
+    remove_unit_loaded_receiver()
+
+    for unit_path in units:
+        # This does nothing if there is no receiver added for this unit
+        remove_properties_changed_receiver(unit_path)
+
+    listening = False
+
+
+def add_unit_loaded_receiver():
+    logging.debug("Adding UnitNew receiver")
     bus.add_signal_receiver(
         handler_function=on_unit_loaded,
         signal_name="UnitNew",
@@ -57,24 +82,43 @@ def start_listening():
         path_keyword='path',
         message_keyword='message',
     )
-    listening = True
-    # This is only needed for debugging, usually the GLib.MainLoop will be started by the GUI
-    # self.loop.run()
 
 
-def stop_listening():
-    global listening
+def remove_unit_loaded_receiver():
     bus.remove_signal_receiver(
         handler_or_match=on_unit_loaded,
         signal_name="UnitNew",
         dbus_interface='org.freedesktop.systemd1.Manager',
         path=None
     )
-    listening = False
+
+
+def add_properties_changed_receiver(unit_path):
+    logging.debug("Adding PropertiesChanged receiver for unit %r", unit_path)
+    bus.add_signal_receiver(
+        handler_function=on_properties_changed,
+        signal_name=None,
+        dbus_interface='org.freedesktop.DBus.Properties',
+        path=unit_path,
+        sender_keyword='sender',
+        destination_keyword='destination',
+        interface_keyword='interface',
+        member_keyword='member',
+        path_keyword='path',
+        message_keyword='message',
+    )
+
+
+def remove_properties_changed_receiver(unit_path):
+    bus.remove_signal_receiver(
+        handler_or_match=on_properties_changed,
+        signal_name=None,
+        dbus_interface='org.freedesktop.DBus.Properties',
+        path=unit_path,
+    )
 
 
 def on_unit_loaded(id, unit_path, member, **kwargs):
-    logging.debug("Got event %r for unit %r", member, unit_path)
     if unit_path not in units:
         return
     unit = units[unit_path]
@@ -83,12 +127,21 @@ def on_unit_loaded(id, unit_path, member, **kwargs):
                   member, unit.name, active_state, sub_state)
 
     unit.receiver_function(active_state, sub_state)
-    # XXX: We have to listen to "SubState" too, because if the systemd unit has
-    # "RemainAfterExit" enabled, then the ActiveState will remain "active" after the service
-    # exited and only the SubState is set to "exited". Now actually, this "RemainAfterExit"
-    # should simply not be enabled for units which run a daemon - but unfortunately, all
-    # systemd units generated from Sys V init files have this set to true.
-    # See https://bugs.launchpad.net/ubuntu/+source/apache2/+bug/1488962/comments/5
+
+
+def on_properties_changed(interface_name, changed_properties, invalidated_properties, path,
+                          member, **kwargs):
+    if path not in units:
+        return
+    if not 'ActiveState' in changed_properties and not 'SubState' in changed_properties:
+        return
+    unit = units[path]
+
+    active_state, sub_state = query_unit_state(unit)
+    logging.debug("Got %r event for unit %r. ActiveState: %r, SubState: %r",
+                  member, unit.name, active_state, sub_state)
+
+    unit.receiver_function(active_state, sub_state)
 
 
 def query_unit_state(unit):
@@ -99,6 +152,13 @@ def query_unit_state(unit):
     # XXX: This will still result in endless UnitNew events if there are multiple processes
     # doing this same thing, e.g. multiple instances of Tails Server.
     # See https://github.com/systemd/systemd/issues/4095
+    #
+    # XXX: We have to query "SubState" too, because if the systemd unit has
+    # "RemainAfterExit" enabled, then the ActiveState will remain "active" after the service
+    # exited and only the SubState is set to "exited". Now actually, this "RemainAfterExit"
+    # should simply not be enabled for units which run a daemon - but unfortunately, all
+    # systemd units generated from Sys V init files have this set to true.
+    # See https://bugs.launchpad.net/ubuntu/+source/apache2/+bug/1488962/comments/5
     stop_listening()
     active_state = unit.properties.Get('org.freedesktop.systemd1.Unit', 'ActiveState')
     sub_state = unit.properties.Get('org.freedesktop.systemd1.Unit', 'SubState')
