3254def2f9e6f5f65a768ffcec78f74ee1e52bbf
segfault <segfault@riseup.net>
2016-10-28 07:33:45 +0000

    Fix exception handling

--- config/chroot_local-includes/usr/lib/python3.4/dist-packages/tails_server/option_util.py
+++ config/chroot_local-includes/usr/lib/python3.4/dist-packages/tails_server/option_util.py
@@ -1,3 +1,5 @@
+import os
+
 from tails_server import service_option_template
 from tails_server import file_util
 
@@ -5,10 +7,11 @@ from tails_server import file_util
 
 
 def get_option(file_path, s):
-    try:
-        line = file_util.find_line_starting_with(file_path, s)
-    except FileNotFoundError as e:
-        raise service_option_template.OptionNotFoundError(e.strerror)
+    if not os.path.exists(file_path):
+        raise service_option_template.OptionNotFoundError("File %r not found" % file_path)
+
+    line = file_util.find_line_starting_with(file_path, s)
+
     if not line:
         raise service_option_template.OptionNotFoundError(
             "Could not find line starting with %r in file %r" % (s, file_path))
