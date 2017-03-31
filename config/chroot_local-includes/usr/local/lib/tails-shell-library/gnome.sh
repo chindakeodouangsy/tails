fe453a97e94a6aedf0b20dbd0e3f714bc483f8d6
anonym <anonym@riseup.net>
2017-03-31 13:41:02 +0200

    gnome.sh: Export more variables via export_gnome_env().
    Hopefully they will help orca making applications accessible.
    
    Refs: #9051

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/gnome.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/gnome.sh
@@ -3,7 +3,7 @@ export_gnome_env() {
     . /etc/live/config.d/username.conf
     local gnome_shell_pid="$(pgrep --newest --euid ${LIVE_USERNAME} gnome-shell)"
     local tmp_env_file="$(tempfile)"
-    local vars="(DBUS_SESSION_BUS_ADDRESS|DISPLAY|XAUTHORITY)"
+    local vars="(DBUS_SESSION_BUS_ADDRESS|DISPLAY|XAUTHORITY|DESKTOP_SESSION|XMODIFIERS|QT_IM_MODULE|SESSION_MANAGER|(GNOME|GDM|XDG)[^=]*)"
     tr '\0' '\n' < "/proc/${gnome_shell_pid}/environ" | \
         grep -E "^${vars}=" > "${tmp_env_file}"
     while read line; do export "${line}"; done < "${tmp_env_file}"
