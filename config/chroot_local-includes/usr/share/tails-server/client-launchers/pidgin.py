74aa730a819154d4dd3b2d027d6bea44f3e0cb0c
anonym <anonym@riseup.net>
2017-04-27 11:32:41 +0200

    Tails Server: add client launcher for Pidgin/Prosody.
    Will-fix: #12478

---
+++ config/chroot_local-includes/usr/share/tails-server/client-launchers/pidgin.py
@@ -0,0 +1,48 @@
+import subprocess
+import os
+
+from tails_server import _
+from tails_server.client_launcher_template import \
+    ClientLauncher, ClientLauncherDetail
+from tails_server.config import HELPER_SCRIPTS_DIR, TAILS_USER
+
+
+class PidginLauncher(ClientLauncher):
+    name = 'pidgin'
+    icon_name = 'pidgin'
+
+    @property
+    def details(self):
+        return super().details + [
+            ClientLauncherDetail('username', _('Username'), str, required=True),
+        ]
+
+    def prepare(self):
+        super().prepare()
+
+    @staticmethod
+    def pidgin_create_account(domain, port, username):
+        account_registration_script = os.path.join(
+            HELPER_SCRIPTS_DIR, 'create-pidgin-xmpp-account'
+        )
+        p = subprocess.Popen([
+            'sudo', '-u', TAILS_USER, 'python3', account_registration_script,
+            domain, port, username
+        ])
+        assert(p.wait() == 0)
+
+    def launch(self):
+        super().launch()
+        account = self.values['username'] + '@' + self.values['address'] + '/'
+        p = subprocess.Popen(
+            ['pgrep', '--euid', TAILS_USER, '--full', '^/usr/bin/pidgin']
+        )
+        pidgin_is_running = p.wait() == 0
+        if not pidgin_is_running:
+            subprocess.Popen(['sudo', '-u', TAILS_USER, 'pidgin'])
+        self.pidgin_create_account(
+            self.values['address'], self.values['port'], self.values['username']
+        )
+
+
+client_launcher_class = PidginLauncher
