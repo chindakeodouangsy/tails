9c43ec75821faec1cae0af2c0ac6d35a21dde2ba
anonym <anonym@riseup.net>
2017-04-28 19:12:00 +0200

    Tails Server: PurpleAccountRegister doesn't work, let's implement the XMPP protocol and register the account ourselves.
    I'm not joking! :)

--- config/chroot_local-includes/usr/share/tails-server/client-launchers/pidgin.py
+++ config/chroot_local-includes/usr/share/tails-server/client-launchers/pidgin.py
@@ -15,33 +15,35 @@ class PidginLauncher(ClientLauncher):
     def details(self):
         return super().details + [
             ClientLauncherDetail('username', _('Username'), str, required=True),
+            ClientLauncherDetail('password', _('Password'), str, required=True),
         ]
 
     def prepare(self):
         super().prepare()
 
     @staticmethod
-    def pidgin_create_account(domain, port, username):
+    def pidgin_create_account(domain, port, username, password):
         account_registration_script = os.path.join(
             HELPER_SCRIPTS_DIR, 'create-pidgin-xmpp-account'
         )
         p = subprocess.Popen([
             'sudo', '-u', TAILS_USER, 'python3', account_registration_script,
-            domain, port, username
+            domain, port, username, password
         ])
-        assert(p.wait() == 0)
 
     def launch(self):
         super().launch()
         account = self.values['username'] + '@' + self.values['address'] + '/'
         p = subprocess.Popen(
-            ['pgrep', '--euid', TAILS_USER, '--full', '^/usr/bin/pidgin']
+            ['pgrep', '--euid', TAILS_USER, '--full', '^/usr/bin/pidgin'],
+            stdout=subprocess.PIPE
         )
         pidgin_is_running = p.wait() == 0
         if not pidgin_is_running:
             subprocess.Popen(['sudo', '-u', TAILS_USER, 'pidgin'])
         self.pidgin_create_account(
-            self.values['address'], self.values['port'], self.values['username']
+            self.values['address'], self.values['port'],
+            self.values['username'], self.values['password']
         )
 
 
