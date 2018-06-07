chindakeodouangsy/tails

commit 9f99bfbca21415ae8db4482c5be39bbfb50ec357

Test suite: Add scenario testing ASP when persistence is locked.
Refs: #14596

features/step_definitions/common_steps.rb
@@ -134,7 +134,7 @@ Given /^I start Tails( from DVD)?( with network unplugged)?( and I login)?$/ do 
   end
 end
 
-Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( and an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on,  admin_password|
+Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with (persistence enabled)?)?( and)?(an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, useless_syntax_with, persistence_on, useless_syntax_and, admin_password|
   step "the computer is set to boot from #{drive_type} drive \"#{drive_name}\""
   if network_unplugged
     step "the network is unplugged"

features/additional_software_packages.feature
@@ -31,6 +31,15 @@ Feature: Additional software packages
     And the additional software package installation service has started
     And the package "sslh" is installed
 
+  @check_tor_leaks
+  Scenario: I can install packages in a Tails session with locked down persistence without being annoyed by ASP
+    Given a computer
+    And I start Tails from USB drive "__internal" and I login with an administration password
+    And I update APT using apt
+    When I install "cowsay" using apt
+    Then ASP has been started for "cowsay" and shuts up because the persistence is locked
+    And the package "cowsay" is installed
+
   #12586
   @check_tor_leaks
   Scenario: Packages I install with Synaptic and add to ASP are automatically installed

features/step_definitions/additional_software_packages.rb
@@ -150,3 +150,10 @@ Then /^I can open the documentation from the notification link$/  do
   # So instead let's try to find the title of the page with Sikuli.
   @screen.wait('ASPDocumentationInstallCloning', 120)
 end
+
+Then /^ASP has been started for "([^"]*)" and shuts up because the persistence is locked$/ do |package|
+  asp_logs = '/run/live-additional-software/log'
+  assert(!$vm.file_empty?(asp_logs))
+  try_for(60) { $vm.execute("grep #{package} #{asp_logs}").success? }
+  try_for(60) { $vm.file_content(asp_logs).include?('Warning: persistence storage is locked') }
+end

commit 0fdec7a7e4237b8fd425e7c50e70029fb3f911fd

Fix bug introduced in commit 9f99bfb. (refs: #14596)

features/step_definitions/common_steps.rb
@@ -134,7 +134,7 @@ Given /^I start Tails( from DVD)?( with network unplugged)?( and I login)?$/ do 
   end
 end
 
-Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with (persistence enabled)?)?( and)?(an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, useless_syntax_with, persistence_on, useless_syntax_and, admin_password|
+Given /^I start Tails from (.+?) drive "(.+?)"( with network unplugged)?( and I login( with persistence enabled)?( (and|with) an administration password)?)?$/ do |drive_type, drive_name, network_unplugged, do_login, persistence_on, and_or_with, admin_password|
   step "the computer is set to boot from #{drive_type} drive \"#{drive_name}\""
   if network_unplugged
     step "the network is unplugged"